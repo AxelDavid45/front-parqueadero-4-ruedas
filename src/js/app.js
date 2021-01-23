39b502b3451b406f91c73f0261210134c733028a
Axel Espinosa <axeldavid45@gmail.com>
2021-01-22 18:06:23 -0600

    feat: function to hadle color tabs

--- src/js/app.js
+++ src/js/app.js
@@ -2,9 +2,24 @@ import { routes } from './utils/routes.js'
 
 const menu = document.getElementById('menu')
 const forms = document.getElementById('forms')
+const resultsApi = document.getElementById('results-api')
 
 menu.addEventListener('click', function (evt) {
   const form = routes(evt.target.id)
+  handleColorTabsMenu(evt)
   forms.innerHTML = form
-  console.log(evt.target.id)
 })
+
+function handleColorTabsMenu(evt) {
+  const elementsUl = evt.target.parentNode.parentNode.childNodes
+  for(const li of elementsUl) {
+    if (li.nodeType === 1) {
+      if (li.classList.contains('is-active')) {
+        li.classList.remove('is-active')
+      }
+    }
+  }
+  if (!evt.target.parentNode.classList.contains('is-active')) {
+    evt.target.parentNode.classList.add('is-active')
+  }
+}
---
+++ src/js/views/cardOwner.js
@@ -0,0 +1,29 @@
+export const cardOwner = (data) => {
+  return `
+  <div class="card">
+      <header class="card-header">
+          <h1 class="card-header-title">
+              Cedula: ${data.cedula}
+          </h1>
+      </header>
+      <div class="card-content">
+          <ul>
+              <li><b>Nombre:</b>${data.nombre}</li>
+              <li><b>-----Vehiculos-----</b></li>
+              ${data.forEach((vehicle) => {
+                return `
+                  <li>
+                  ---
+                    <ul>
+                        <li><b>Placa:</b> ${vehicle.placa}</li>
+                        <li><b>Marca:</b>${vehicle.marca}</li>
+                        <li><b>Tipo:</b>${vehicle.tipo}</li>
+                    </ul>
+                  ---
+                  </li>`
+              })}
+          </ul>
+      </div>
+    </div>
+  `
+}
